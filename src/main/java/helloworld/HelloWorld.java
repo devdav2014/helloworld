fe572e6486d37f76dc8455293925f132047316ed
devdav2014 <Start12345>
2014-11-26 15:05:43 +0100

    JUnit test

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+/**
+ * @author Thomas Aagaard Jensen (z3thj), KMD A/S
+ * @author Morten Furbo Skødt Nielsen <mtn@kmd.dk>, KMD A/S
+ * Date: 26-11-14
+ */
+public class HelloWorld {
+    public String toString() {
+        return "HelloWorld";
+    }
+}
